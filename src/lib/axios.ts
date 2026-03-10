62b8bac235fd612e1a08e906abe894d216025151
ShaNaim <shanaim2k15@gmail.com>
2026-03-10 23:05:49 +0600

    :wrench: added axios configuration

---
+++ src/lib/axios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+})
+
+// Request interceptor
+api.interceptors.request.use((config) => {
+  return config
+})
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle unauthenticated - redirect to login
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
