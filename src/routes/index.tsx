1814e98d63ab39c13c92d3797346f06d11e28515
ShaNaim <shanaim2k15@gmail.com>
2026-03-10 20:59:24 +0600

    :sparkles: initialized tenstack router

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return <div><h1>Home</h1></div>
+}
