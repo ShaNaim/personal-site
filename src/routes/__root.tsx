8f1ab31dd4386ada9b7bb135d8710ad1a91baf20
ShaNaim <shanaim2k15@gmail.com>
2026-03-11 06:20:38 +0600

    :sparkles: added theme

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Outlet />
       <Toaster richColors position="top-right" />
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 });
