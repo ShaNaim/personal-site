9797620b13701830066b9b617108212705e9c1a4
ShaNaim <shanaim2k15@gmail.com>
2026-04-17 00:56:59 +0600

    :bug: removed double footer in portfolio page

--- src/components/modules/portfolio/index.tsx
+++ src/components/modules/portfolio/index.tsx
@@ -1,15 +1,12 @@
-import { CursorOverlay } from "@/components/templates/cursor-overlay";
 import { HeroSection } from "@/components/modules/portfolio/hero-section";
 import { ExperienceSection } from "@/components/modules/portfolio/experience-section";
 import { SkillsSection } from "@/components/modules/portfolio/skills-section";
 import { HobbiesSection } from "@/components/modules/portfolio/hobbies-section";
 import { ContactSection } from "@/components/modules/portfolio/contact-section";
-import { Footer } from "@/components/common/footer";
 
 export default function Portfolio() {
   return (
     <div className="bg-(--color-bg) text-(--color-text) min-h-screen font-mono overflow-x-hidden cursor-none">
-      <CursorOverlay />
       <main>
         <HeroSection />
         <ExperienceSection />
@@ -17,7 +14,6 @@ export default function Portfolio() {
         <HobbiesSection />
         <ContactSection />
       </main>
-      <Footer />
     </div>
   );
 }
